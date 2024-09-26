78fdcbf101f5d9c91bb64dae45e7408482e59fba
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2024-09-26 12:53:17 -0500

    fix(livestreams youtube): they changed things and it broke our things :) - closes #9

--- test/pista/html_parsers_live_test.exs
+++ test/pista/html_parsers_live_test.exs
@@ -34,4 +34,50 @@ defmodule HTMLParsersLiveTest do
                Pista.HTMLParsers.parse_live_redbull_tv(%{html_input: html})
     end
   end
+
+  describe "YouTube Livestream check" do
+    test "returns a data structure for youtube channel if it is livestreaming" do
+      {:ok, html} = read_file("youtube_live.html")
+
+      assert [%{video_id: "mH-s2G8KIpc", channel_name: "@A1PADEL"}] =
+               Pista.HTMLParsers.parse_live_youtube_channel(%{
+                 html_input: html,
+                 channel: "@A1PADEL"
+               })
+    end
+
+    test "returns a data structure if a channel is live and has multiple live videos" do
+      {:ok, html} = read_file("youtube_live_multiple.html")
+
+      assert [_, _] =
+               _only_two =
+               Pista.HTMLParsers.parse_live_youtube_channel(%{
+                 html_input: html,
+                 channel: "@RelaxationChannel"
+               })
+    end
+
+    test "does not return anything if the returns a data structure for youtube channel that if it is livestreaming" do
+      {:ok, html} = read_file("youtube_not_live.html")
+
+      assert [] =
+               Pista.HTMLParsers.parse_live_youtube_channel(%{
+                 html_input: html,
+                 channel: "@Premier"
+               })
+    end
+  end
+
+  defp read_file(file_name) do
+    [
+      File.cwd!(),
+      "test",
+      "support",
+      "fixtures",
+      "static_html",
+      file_name
+    ]
+    |> Path.join()
+    |> File.read()
+  end
 end
