f0412ee197df3a25b4af05c0e0502803f3811806
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2024-10-01 14:46:45 -0500

    Rm register for now

--- lib/pista_web/live/user_confirmation_live.ex
+++ lib/pista_web/live/user_confirmation_live.ex
@@ -16,8 +16,7 @@ defmodule PistaWeb.UserConfirmationLive do
       </.simple_form>
 
       <p class="text-center mt-4">
-        <.link href={~p"/users/register"}>Register</.link>
-        | <.link href={~p"/users/log_in"}>Log in</.link>
+        <.link href={~p"/users/log_in"}>Log in</.link>
       </p>
     </div>
     """
--- lib/pista_web/live/user_forgot_password_live.ex
+++ lib/pista_web/live/user_forgot_password_live.ex
@@ -20,8 +20,7 @@ defmodule PistaWeb.UserForgotPasswordLive do
         </:actions>
       </.simple_form>
       <p class="text-center text-sm mt-4">
-        <.link href={~p"/users/register"}>Register</.link>
-        | <.link href={~p"/users/log_in"}>Log in</.link>
+        <.link href={~p"/users/log_in"}>Log in</.link>
       </p>
     </div>
     """
